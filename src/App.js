5767be4328f6713dc81ab72f7defbfe09712d739
Aristóteles Núñez <aristoteles.nunez@gmail.com>
2020-02-03 20:14:00 -0600

    feat: Basic Nav Bar with theme color

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
 import AppNavBar from './AppNavBar';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { blue, amber } from '@material-ui/core/colors';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: amber,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <AppNavBar />
-      <p> Hello World</p>
-      
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <AppNavBar title={'My Reads'}/>        
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/AppNavBar.js
+++ src/AppNavBar.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-const appNavBar = (props) => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const AppNavBar = (props) => {
+    const {title} = props;
+    const classes = useStyles();
     return (
-        <AppBar>
-            <h2>My reads</h2>
-        </AppBar>
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title}>
+                        {title}
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
     );    
 };
 
-export default appNavBar;
+export default AppNavBar;
