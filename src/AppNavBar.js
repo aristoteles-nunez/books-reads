9e24651328272c3842af08ddba36c0ff5be39cf1
Aristóteles Núñez <aristoteles.nunez@gmail.com>
2020-02-06 18:28:55 -0600

    feat: Addded button to add a new book

--- src/AppNavBar.js
+++ src/AppNavBar.js
@@ -4,19 +4,30 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
+    alignItems: 'center',
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
-    flexGrow: 1,
+    width: 300
   },
   bar: {
-    backgroundColor: '#115293'
+    backgroundColor: '#115293',
+    alignItems: 'center',
+  },
+  margin: {
+    
+  },
+  tool: {
+    flexGrow: 1,
+    alignItems: 'center',
   }
 }));
 
@@ -24,12 +35,15 @@ const AppNavBar = (props) => {
     const {title} = props;
     const classes = useStyles();
     return (
-        <div className={classes.root}>
-            <AppBar className={classes.bar} position="static">
-                <Toolbar>
+        <div align="center" className={classes.root}>
+            <AppBar className={classes.bar} position="relative">
+                <Toolbar className={classes.tool} position="relative">
                     <Typography variant="h6" className={classes.title}>
                         {title}
                     </Typography>
+                    <Fab color="secondary" aria-label="add" className={classes.margin}>
+                      <AddIcon />
+                    </Fab>
                 </Toolbar>
             </AppBar>
         </div>
